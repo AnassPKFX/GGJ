5d56b7f0399a73cb5cc7e9a90b9466ac6bd4a387
Ema Ouardi <emaouardi@gmail.com>
2024-01-27 12:20:51 +0100

    [FIX] merge + Import TMP Essential

---
+++ GGJ_AutoBattle/Assets/Scripts/HumourTypeData.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static GameData;
+
+[CreateAssetMenu(fileName = "HumourTypeData", menuName = "GGJ_Datas/HumourTypeData", order = 2)]
+public class HumourTypeData : ScriptableObject
+{
+    public EHumourType HumourType { get => _humourType; set => _humourType = value; }
+    [SerializeField]
+    private EHumourType _humourType;
+
+    public string DisplayName { get => _displayName; set => _displayName = value; }
+    [SerializeField]
+    private string _displayName;
+
+
+}
